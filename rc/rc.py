bc089051a6e0876ea15b7ba344a3bfbd85833ac8
Chema Ajenjo <5195859+monoetharyus@users.noreply.github.com>
2018-03-31 14:31:14 +0200

    Update rc.py

--- rc/rc.py
+++ rc/rc.py
@@ -15,12 +15,6 @@
 
 from bottle import post, route, request, template, static_file, run
 import os, subprocess
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(21, GPIO.OUT)
-GPIO.output(21, True)
 
 @route('/')
 @route('/', method='POST')
